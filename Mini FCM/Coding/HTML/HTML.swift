900694d94f8723bdbeb8ae6ed05234e82386f50f
FridaDEV <157828245+fridakitten@users.noreply.github.com>
2024-12-07 17:44:28 +0100

    adding WebView

---
+++ Mini FCM/Coding/HTML/HTML.swift
@@ -0,0 +1,15 @@
+import UIKit
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let htmlContent: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.loadHTMLString(htmlContent, baseURL: nil)
+    }
+}
